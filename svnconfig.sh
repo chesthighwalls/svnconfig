4df77db8e73013d03d04b1e262e1959d5fb8d71e
chesthighwalls <seanreeves@gmail.com>
2018-04-09 20:48:07 -0400

    Initial commit

---
+++ svnconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author: Sean Reeves
+# Date Created: 04/06/2018
+# Date Modified:
+# Description: A simple bash script to reconfigure svnserve.conf files for several repositories at once.
+# License: MIT License
+# Reference:
+UUID=$(cat /proc/sys/kernel/random/uuid);
+PWD=$(pwd);
+FNAME=$(tempfile);
+#[Sean Reeves] (04/06/2018) Comment: Create config file
+echo "anon-access=none\nauth-access=write\npassword-db=$PWD/passwd\nrealm=$UUID\n" >> "$FNAME";
+#[Sean Reeves] (04/06/2018) Comment: Comment out any already existing lines, and add config lines to each file
+find . -name "svnserve.conf" -exec sed -i.bak "s|^\([^#\s\t]\)|#\1|m" {} \; -exec sh -c "cat $FNAME >> {}" \;
+#[Sean Reeves] (04/06/2018) Comment: Cleanup
+rm $FNAME;
