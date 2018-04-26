7ef2c654b8d06151a1c7b3d1022644755da344d9
chesthighwalls <seanreeves@gmail.com>
2018-04-25 20:38:48 -0400

    Added expected section header

--- svnconfig.sh
+++ svnconfig.sh
@@ -9,7 +9,7 @@ UUID=$(cat /proc/sys/kernel/random/uuid);
 PWD=$(pwd);
 FNAME=$(tempfile);
 #[Sean Reeves] (04/06/2018) Comment: Create config file
-echo "anon-access=none\nauth-access=write\npassword-db=$PWD/passwd\nrealm=$UUID\n" >> "$FNAME";
+echo "[general]\nanon-access=none\nauth-access=write\npassword-db=$PWD/passwd\nrealm=$UUID\n" >> "$FNAME";
 #[Sean Reeves] (04/06/2018) Comment: Comment out any already existing lines, and add config lines to each file
 find . -name "svnserve.conf" -exec sed -i.bak "s|^\([^#\s\t]\)|#\1|m" {} \; -exec sh -c "cat $FNAME >> {}" \;
 #[Sean Reeves] (04/06/2018) Comment: Cleanup
